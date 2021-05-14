af93ba1ccfd374cf6b5921a9038b2b3eeb4a0dfe
Deberson de Paula <deberson.paula@gmail.com>
2021-05-14 16:26:43 -0300

    fixed main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import HeaderBar from './components/HeaderBar';
-
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import HeaderBar from './components/HeaderBar';
+import PostBoard from './components/PostBoard';
 
 const theme = createMuiTheme({});
 Amplify.configure(config);
@@ -16,6 +16,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <HeaderBar />
+      <PostBoard />
     </ThemeProvider>
   );
 }
