efa9570aa94bbaba21d43639c56ea9d686b0251b
Deberson de Paula <deberson.paula@gmail.com>
2021-05-15 16:16:24 -0300

    fixed PostCreate

--- src/components/PostCreate.tsx
+++ src/components/PostCreate.tsx
@@ -1,22 +1,24 @@
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
 import CreatePostService from '../services/CreatePostService';
-import { DynamicForm } from './DynamicForm';
+import DynamicForm from './DynamicForm';
 import Loader from './Loader';
+import Typography from '@material-ui/core/Typography';
 
-export default function PostCreate() {
+export default function PostCreate(props: IProps) {
   const postCreation = CreatePostService();
 
   const handleOnSubmit = (values: any) => {
-    if (postCreation.save) {
-      postCreation.save(values);
-    }
+    postCreation.insertPost(values).then((postId) => {
+      if (postId) {
+        props.onCreatePost(postId);
+      }
+      props.onClose();
+    });
   };
 
   return (
-    <Card>
+    <Card style={{ position: 'relative' }}>
       <CardContent>
         <Typography variant="h5" component="h2">
           Create a post:
@@ -31,9 +33,22 @@ export default function PostCreate() {
             { label: 'Image', name: 'image', type: 'file' },
           ]}
           onSubmit={handleOnSubmit}
+          onCancel={props.onClose}
         />
       </CardContent>
-      <Loader />
+      {postCreation.isSaving && <Loader overrideParent={true} />}
+      {postCreation.isFailed && (
+        <CardContent>
+          <Typography variant="h5" component="h2" style={{ color: 'red' }}>
+            Error on post saving. Try again!!!
+          </Typography>
+        </CardContent>
+      )}
     </Card>
   );
+}
+
+interface IProps {
+  onClose: () => void;
+  onCreatePost: (postId: string) => void;
 }
